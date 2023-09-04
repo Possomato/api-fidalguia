bef84e82c6f861b721aa61bc7c14549798fe509e
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-04 14:43:11 -0300

    add createUser and upadatUser in UsersController

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,14 +4,9 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next){
-  console.log("você passou pelo middleware")
-
-  next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post('/', myMiddleware, usersController.create)
+usersRoutes.post('/', usersController.create)
+usersRoutes.put('/:id', usersController.update)
 
 module.exports = usersRoutes
