289a08ec43af36e2d7fee66bd2a58851d2bb5622
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-07 10:57:22 -0300

    add authentication by jwt

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -3,7 +3,7 @@ const knex = require('../database/knex')
 class NotesController{
   async create(request, response){
     const { title, description, tags, links } = request.body
-    const { user_id } = request.params
+    const user_id = request.user.id
 
     const noteIds = await knex('notes').insert({
       title,
@@ -58,7 +58,9 @@ class NotesController{
   }
 
   async index(request, response){
-    const { user_id, title, tags } = request.query
+    const { title, tags } = request.query
+    const user_id = request.user.id
+
 
     let notes
 
